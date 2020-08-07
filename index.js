dc7e55f5fd894d58fd7ae18eb84bc3061a104099
Cusyas <jdleach@outlook.com>
2020-08-07 16:06:01 -0400

    Added main pages and embedded pdf version of resume

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ const express = require('express'),
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/images'));
 
+// INDEX ROUTE
 app.get('/', (req, res) => {
 	res.render('landingpage');
 });
+
+// ABOUT ME SHOW ROUTE
+app.get('/aboutme', (req, res) => {
+	res.render('aboutme');
+});
+
+// RESUME SHOW ROUTE
+app.get('/resume', (req, res) => {
+	res.render('resume');
+});
+
+// Wildcard route
 
 app.listen(3000, '127.0.0.1', function() {
 	console.log('server online');
